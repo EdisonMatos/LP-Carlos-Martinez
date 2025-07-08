cb36924e4c351c1fe0d1d9cbac2f48a1d39449f5
Gabriel de Souza da Silva <gabrielsouza65115@gmail.com>
2025-07-07 21:51:33 -0300

    fix: Ajustes gerais logo

--- src/components/sections/NavbarSocial.jsx
+++ src/components/sections/NavbarSocial.jsx
@@ -116,7 +116,7 @@ export default function NavbarSocial({ colorMode }) {
               className={`bg-transparent max-h-[90px] ${
                 scrolling
                   ? " bg-white p-2 rounded-md w-[50%] phone3:w-[40%] tablet1:w-[40%] tablet2:w-[40%] desktop1:w-[50%] desktop2:w-[60%]"
-                  : "bg-white p-2 my-[20px] w-[80%] phone2:w-[70%] phone3:w-[60%] tablet1:w-[60%] tablet2:w-[70%] desktop2:w-[80%]"
+                  : "bg-white p-2 rounded-md my-[20px] w-[80%] phone2:w-[70%] phone3:w-[60%] tablet1:w-[60%] tablet2:w-[70%] desktop2:w-[80%]"
               } transition-all duration-1000`}
             />
           </ScrollLink>
