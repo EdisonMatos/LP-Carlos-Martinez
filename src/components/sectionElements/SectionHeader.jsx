d6b04a6b58ea4b084cd3ab10c373a1d57ab4661a
Gabriel de Souza da Silva <gabrielsouza65115@gmail.com>
2025-07-09 11:09:30 -0300

    fix: tag com letra minúscula no about

--- src/components/sectionElements/SectionHeader.jsx
+++ src/components/sectionElements/SectionHeader.jsx
@@ -60,7 +60,7 @@ export default function SectionHeader({
           <div
             className={`py-[4px] font-semibold px-[12px] font-mainFont text-paragraph2 rounded-md inline-block mb-[16px] ${miniTitleOrientation} ${miniTitleBgColor}`}
           >
-            <p className={`${miniTitleTextColor} uppercase`}>{miniTitle}</p>
+            <p className={`${miniTitleTextColor} `}>{miniTitle}</p>
           </div>
         </div>
         <h1
